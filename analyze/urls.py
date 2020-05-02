a895618a1da1e04b70cd2a05d37158cc217cd0b0
selimlouis <selimseyhanoglu@gmail.com>
2020-05-02 18:59:26 +0200

    added update and delete

--- analyze/urls.py
+++ analyze/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import ReportDetailView, ReportCreateView
+from .views import (ReportDetailView, 
+            ReportCreateView, 
+            ReportUpdateView,
+            ReportDeleteView
+)
 
 
 
@@ -9,4 +13,6 @@ urlpatterns = [
     path('', views.index, name = "index"),
     path('report/<int:pk>/', ReportDetailView.as_view(), name="report-detail"),
     path('report/new/', ReportCreateView.as_view(), name="report-create"),
+    path('report/<int:pk>/update/', ReportUpdateView.as_view(), name="report-update"),
+    path('report/<int:pk>/delete/', ReportDeleteView.as_view(), name="report-delete")
 ]
--- analyze/views.py
+++ analyze/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.template import loader
-from django.views.generic import DetailView, CreateView
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
 from .models import Report
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from django.http import HttpResponse
 
@@ -17,8 +18,20 @@ def index(request):
 class ReportDetailView(DetailView):
     model = Report
 
+class ReportDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Report
+    success_url = '/profile'
 
-class ReportCreateView(CreateView):
+    def test_func(self):
+        report = self.get_object()
+        if report.author == self.request.user:
+            return True
+
+        return False
+
+
+#login mixin is a super class that handles the login requirement
+class ReportCreateView(LoginRequiredMixin, CreateView):
     model = Report
     fields = ["headline", "content"]
 
@@ -26,3 +39,22 @@ class ReportCreateView(CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class ReportUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Report
+    fields = ["headline", "content"]
+
+    #link the report to the request user
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    #make sure the logged in user is the author of the report
+    def test_func(self):
+        #get the report that is being edited on
+        report = self.get_object()
+        if self.request.user == report.author:
+            return True
+        
+        return False
