00e82bc46a783eff9b912b1fe5542101a0712df7
selimlouis <selimseyhanoglu@gmail.com>
2020-05-06 21:55:27 +0200

    greatly improved styling

--- users/models.py
+++ users/models.py
@@ -11,13 +11,13 @@ class Profile(models.Model):
         return f"{self.user.username} Profile"
     
 
-    def save(self):
+    def save(self, *args, **kwargs):
         #parent save method
         super().save()
 
         img = Image.open(self.image.path)
 
-        if img.height > 300 or img.width > 300:
+        if img.height >= 300 or img.width >= 300:
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
