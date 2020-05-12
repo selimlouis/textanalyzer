10b2f4bdfc209b433ead9a6df31acb10d0a6c0f8
selimlouis <selimseyhanoglu@gmail.com>
2020-05-12 23:39:07 +0200

    added responsive nav bar

--- analyze/static/analyze/main.js
+++ analyze/static/analyze/main.js
@@ -128,3 +128,13 @@ function analyzeText(text){
 
 
 }
+
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+}
